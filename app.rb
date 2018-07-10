e44383f564e4c9fe0fa301ba8d5431a59d8de40b
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-10 16:24:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,4 +9,13 @@ class App < Sinatra::Base
   get '/new' do
     erb :create_puppy
   end
+
+  post '/' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+
+    erb :display_puppy
+  end
+
 end
